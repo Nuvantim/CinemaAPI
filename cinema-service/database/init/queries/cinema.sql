b10d176e1c1fd8bc90d39273edc192bd44a53f26
Nuvantim <71752646+Nuvantim@users.noreply.github.com>
2025-11-03 09:34:02 +0700

    Update(database): Add query to list cinema schedules
    Added a new query to list cinema schedules with details.

--- cinema-service/database/init/queries/cinema.sql
+++ cinema-service/database/init/queries/cinema.sql
@@ -1,6 +1,22 @@
 -- name: ListCinema :many
 SELECT * FROM cinema;
 
+-- name: ListCinemaSchedule :many
+SELECT
+    c.name AS cinema_name,
+    f.title AS film_title,
+    st.name AS screen_type,
+    sc.name AS screen_name,
+    sh.start_time,
+    sh.base_price
+FROM showtime sh
+INNER JOIN film f ON sh.film_id = f.id
+INNER JOIN screen sc ON sh.screen_id = sc.id
+INNER JOIN screen_type st ON sc.screen_type_id = st.id
+INNER JOIN cinema c ON sc.cinema_id = c.id
+WHERE c.id = $1
+ORDER BY sh.start_time ASC;
+
 -- name: CreateCinema :one
 INSERT INTO cinema (name,address,city) VALUES ($1,$2,$3) RETURNING *;
 
