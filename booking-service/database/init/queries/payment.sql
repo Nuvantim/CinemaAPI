fdc40ebd2f97751a7d1f391bf0d35143c91d4d92
Nuvantim <71752646+Nuvantim@users.noreply.github.com>
2025-11-03 09:37:00 +0700

    Update(database): Refactor payment SQL query to use sqlc arguments

--- booking-service/database/init/queries/payment.sql
+++ booking-service/database/init/queries/payment.sql
@@ -2,12 +2,12 @@
 INSERT INTO payment (booking_id, payment_method, payment_status, transaction_amount, payment_time)
 SELECT 
     b.id,
-    $2 AS payment_method,
-    "Success" AS payment_status,
+    sqlc.arg(payment_method) AS payment_method,
+    'Success' AS payment_status,
     b.total_amount AS transaction_amount,
     NOW() AS payment_time
-FROM booking
-WHERE booking.id = $1
+FROM booking b
+WHERE b.id = sqlc.arg(booking_id)
 RETURNING *;
 
 -- name: ListPayment :many
